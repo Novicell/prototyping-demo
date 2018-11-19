b20b12b35d7db02f1b094c017387bf5fc85e70a7
Brian Stefan Jensen <bsj@novicell.dk>
2018-11-19 15:15:05 +0100

    Getting demo ready

--- assets/scripts/master.js
+++ assets/scripts/master.js
@@ -9,16 +9,7 @@ var projectName = projectName || {};
 
 // Document ready
 document.addEventListener('DOMContentLoaded', function(){
-    novicell.validate.init(); // Init Validation
-    novicell.inputMasking.init(); //Init input masking
-    novicell.persistentField.init(); //Init persistentField
-    novicell.topbarRelated.init(); 
-    novicell.pageheaderSlider.init();
-    novicell.cookieInfo.init(); 
-    novicell.scrollProgressBar.init();
-    novicell.navigation.init();
-    novicell.navTrigger.init();
-    novicell.pageheaderVideoYoutube.init();
+    novicell.pageheaderDots.init();
 
     //SetTimeout to fix latency with loaded elements in e.g. Vue components
     setTimeout(() => {
